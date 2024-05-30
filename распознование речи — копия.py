44e64d97c0155351565b9b20e9b465143bea8497
Yaroskuf <160023463+Yaroskuf@users.noreply.github.com>
2024-05-30 13:25:26 +0300

    Add files via upload

--- распознование речи — копия.py
+++ распознование речи — копия.py
@@ -2,6 +2,14 @@ import keyboard
 import speech_recognition as sr
 import pywhatkit as kit
 
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
+
+
 # Номер получателя и сообщение
 sergay = '+79670331777'
 timofey='+79268403751'
@@ -82,6 +90,22 @@ while True:
         keyboard.send('alt+f4')
     if 'следующая вкладка' in text:
         keyboard.send('alt+tab')
+    if 'запрос' in text:
+        text=text.replace('запрос', '')
+        # Настройка драйвера Chrome
+        service = Service(ChromeDriverManager().install())
+        driver = webdriver.Chrome(service=service)
+        # Открыть страницу Google
+        driver.get('https://www.google.com')
+        # Найти элемент ввода (строка поиска)
+        search_box = driver.find_element(By.NAME, 'q')
+        # Ввести запрос в строку поиска
+        query = text
+        search_box.send_keys(query)
+        # Отправить запрос (нажать Enter)
+        search_box.send_keys(Keys.RETURN)
+        # Дополнительно: дождаться загрузки результатов и вывести их заголовки
+        driver.implicitly_wait(10)  # Ожидание до 10 секунд, пока результаты загрузятся
     if 'отправить' in text:
         keyboard.send('enter')
     if 'стоп' in text:
